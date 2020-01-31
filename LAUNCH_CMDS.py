f538222a4cbb009f66afbd5354018dcf5838f29d
Pablo Prieto <prietop@unican.es>
2020-01-31 10:47:58 +0100

    Modified imagick launch command to reduce the time to reach the barrier

--- LAUNCH_CMDS.py
+++ LAUNCH_CMDS.py
@@ -58,7 +58,8 @@ deepsjeng = {
     1 : "ref.txt"
 }
 imagick = {
-    1 : "-limit disk 0 refrate_input.tga -edge 41 -resample 181% -emboss 31 -colorspace YUV -mean-shift 19x19+15% -resize 30% refrate_output.tga"
+    #1 : "-limit disk 0 refrate_input.tga -edge 41 -resample 181% -emboss 31 -colorspace YUV -mean-shift 19x19+15% -resize 30% refrate_output.tga"
+    1 : "-limit disk 0 refrate_input.tga -mean-shift 19x19+15% -edge 41 -resample 181% -emboss 31 -colorspace YUV -resize 30% refrate_output.tga"
 }
 leela = {
     1 : "ref.sgf"
