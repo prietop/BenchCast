b4e4c8151a2319dfe722dcbb7c806d55fa47b1da
Pablo Prieto <prietop@unican.es>
2020-06-17 13:12:18 +0200

    Error detection in args and print parenthesis

--- launch_bench.py
+++ launch_bench.py
@@ -28,16 +28,16 @@ def checkFile(dir_to_search, file_to_check):
 def printColor(string_to_print,color):
     """Prints messages to stdout of a defined color"""
     if color== "green":
-        print bcolors.OKGREEN + string_to_print + bcolors.ENDC
+        print(bcolors.OKGREEN + string_to_print + bcolors.ENDC)
     elif color== "red":
-        print bcolors.FAIL + string_to_print + bcolors.ENDC
+        print(bcolors.FAIL + string_to_print + bcolors.ENDC)
     else:
-        print bcolors.FAIL + "Bad color argument to print" + bcolors.ENDC
+        print(bcolors.FAIL + "Bad color argument to print" + bcolors.ENDC)
 
 def runCommand(command, inputVal=''):
     """Run a shell command"""
     if (inputVal == ''):
-       print command
+       print(command)
     ret_code=subprocess.call(command, shell=True)
     if ret_code:
         printColor(("bad return code for command execution: %s" % ret_code), "red")
@@ -48,13 +48,13 @@ def runCommand(command, inputVal=''):
 """ Script main function """
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--cmd', type=str, default="1",
-                    help='Benchmark number')
+                    help='Benchmark number/letter... (is A, bwaves 2)')
 parser.add_argument('--app', type=str, action="store",
-                    help="Name of the benchmark app to launch")
+                    help="Name of the benchmark app to launch (is, ft, bwaves, mcf...)")
 parser.add_argument('--conf', type=str, action="store", 
-                    help='Name of the configuration file (only on spec)')
+                    help='Name of the configuration file (only with SPEC2017)')
 parser.add_argument('--bench', type=str, action="store",
-                    help="Name of the benchmark the app belongs to")
+                    help="Name of the benchmark the app belongs to (SPEC, PARSEC, NPB...)")
 
 
 args = parser.parse_args()
@@ -62,18 +62,28 @@ config_name="gem5"
 if args.conf:
     config_name=args.conf
 
-print "using config file %s" % config_name
+print("using config file %s" % config_name)
 original_dir=os.getcwd()
 
+if args.app is None:
+    printColor("ERROR --app needed and not included", "red")
+    parser.print_help(sys.stderr)
+    exit()
+if args.cmd is None:
+    printColor("ERRPR --cmd needed and not included", "red")
+    parser.print_help(sys.stderr)
+    exit()
 if (args.bench == "SPEC"):
     app = SPEC(args.app, args.conf)
 elif (args.bench == "NPB"):
     app = NPB(args.app)
 else:
     printColor("Benchmark type %s not included" % args.bench, "red")
+    parser.print_help(sys.stderr)
     exit()
+
 exec_dir=app.getExecPath()
-print exec_dir
+print(exec_dir)
 checkDir(exec_dir)
 exec_cmd=app.getExecCmd(args.cmd)
 os.chdir(exec_dir)
